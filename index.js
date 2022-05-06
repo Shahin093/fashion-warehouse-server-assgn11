650adb506d2c777272760f82b01720316c8fab0c
Shidul Islam <sishahin093@gmail.com>
2022-05-06 18:03:11 +0600

    completing backend data load and show ui desplay

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// user name  : fasion-1    fasion-inventory
+// pass : yvZAomT8eX9FNVrN   Helc5DfWJkIK8Reu
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://fasion-inventory:Helc5DfWJkIK8Reu@cluster-fasion.4ijhi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        client.connect();
+        const fashionCollection = client.db("fasionService").collection("service");
+        // api data load
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = fashionCollection.find(query);
+            const fasion = await cursor.toArray();
+            res.send(fasion);
+        });
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
